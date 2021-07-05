e0653b3634a8f9361a94778f2109756cd41b800e
Gonzalo <gon.pineiro@gmail.com>
2021-07-05 08:29:26 -0300

    :hammer: UserSeeder

--- index.php
+++ index.php
@@ -2,26 +2,7 @@
 include 'app/config/config.php';
 
 
-/* $userController = new UsuarioController();
-$user = [
-    'id_wappersonas' =>  2,
-    'nombre' => 'Gonzalo',
-    'apellido' => 'Gonzalo',
-    'telefono' => '1126410253',
-    'dni' => 31960202,
-    'genero' => 'H',
-    'email' => 'gon.pineiro@gmail.com',
-    'nacionalidad' => 'Argentino',
-    'id_ciudad' => 1,
-    'id_barrio' => 1,
-    'id_zona' => 3,
-    'direccion_calle' => 'Pinar',
-    'direccion_nro' => '572',
-    'direccion_depto' => 'SI',
-    'direccion_piso' => '8300',
-    'direccion_cp' => '8300',
-];
-$userController->store($user);
+
 
 $solController = new SolicitudController();
 $sol = [
@@ -67,7 +48,7 @@ $bar = [
     'id_ciudad' => 52,
     'nombre' => 'Barrio magica de BsAs',
 ];
-$barrioController->store($bar); */
+$barrioController->store($bar);
 
 $solicitud = new SolicitudController();
 $listado = $solicitud->getSolicitudesWhereEstado('Estro es un estado magico');
