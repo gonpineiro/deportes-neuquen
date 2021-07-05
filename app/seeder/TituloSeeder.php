bcd3bf01258020be9cae446b2b14d84151e1826f
Gonzalo <gon.pineiro@gmail.com>
2021-07-05 08:48:36 -0300

    :hammer: TituloSeeder

---
+++ app/seeder/TituloSeeder.php
@@ -0,0 +1,80 @@
+<?php
+
+$tituloController = new TituloController();
+
+$tituloController->store([
+    'id_solicitud' => 1,
+    'titulo' => 'Esto es un titulo 1 Solicitud 1',
+    'foto_titulo' => 'EESTO ES UNA FOTO SUPER BASE 64',
+    'es_curso' => 1,
+]);
+
+$tituloController->store([
+    'id_solicitud' => 1,
+    'titulo' => 'Esto es un titulo 2 Solicitud 1',
+    'foto_titulo' => 'EESTO ES UNA FOTO SUPER BASE 64',
+    'es_curso' => 1,
+]);
+
+$tituloController->store([
+    'id_solicitud' => 1,
+    'titulo' => 'Esto es un titulo 3 Solicitud 1',
+    'foto_titulo' => 'EESTO ES UNA FOTO SUPER BASE 64',
+    'es_curso' => 1,
+]);
+
+$tituloController->store([
+    'id_solicitud' => 2,
+    'titulo' => 'Esto es un titulo 1 Solicitud 2',
+    'foto_titulo' => 'EESTO ES UNA FOTO SUPER BASE 64',
+    'es_curso' => 1,
+]);
+
+$tituloController->store([
+    'id_solicitud' => 2,
+    'titulo' => 'Esto es un titulo 2 Solicitud 2',
+    'foto_titulo' => 'EESTO ES UNA FOTO SUPER BASE 64',
+    'es_curso' => 1,
+]);
+
+$tituloController->store([
+    'id_solicitud' => 3,
+    'titulo' => 'Esto es un titulo 1 Solicitud 3',
+    'foto_titulo' => 'EESTO ES UNA FOTO SUPER BASE 64',
+    'es_curso' => 1,
+]);
+
+$tituloController->store([
+    'id_solicitud' => 3,
+    'titulo' => 'Esto es un titulo 2 Solicitud 3',
+    'foto_titulo' => 'EESTO ES UNA FOTO SUPER BASE 64',
+    'es_curso' => 1,
+]);
+
+$tituloController->store([
+    'id_solicitud' => 4,
+    'titulo' => 'Esto es un titulo 1 Solicitud 4',
+    'foto_titulo' => 'EESTO ES UNA FOTO SUPER BASE 64',
+    'es_curso' => 1,
+]);
+
+$tituloController->store([
+    'id_solicitud' => 4,
+    'titulo' => 'Esto es un titulo 2 Solicitud 4',
+    'foto_titulo' => 'EESTO ES UNA FOTO SUPER BASE 64',
+    'es_curso' => 1,
+]);
+
+$tituloController->store([
+    'id_solicitud' => 5,
+    'titulo' => 'Esto es un titulo 1 Solicitud 5',
+    'foto_titulo' => 'EESTO ES UNA FOTO SUPER BASE 64',
+    'es_curso' => 1,
+]);
+
+$tituloController->store([
+    'id_solicitud' => 5,
+    'titulo' => 'Esto es un titulo 2 Solicitud 5',
+    'foto_titulo' => 'EESTO ES UNA FOTO SUPER BASE 64',
+    'es_curso' => 1,
+]);
--- index.php
+++ index.php
@@ -6,14 +6,7 @@ include 'app/config/config.php';
 
 
 
-$tituloController = new TituloController();
-$titu = [
-    'id_solicitud' => 1,
-    'titulo' => 'Esto es un titulo No universitario',
-    'foto_titulo' => 'EESTO ES UNA FOTO SUPER BASE 64',
-    'es_curso' => 1,
-];
-$tituloController->store($titu);
+
 
 
 
