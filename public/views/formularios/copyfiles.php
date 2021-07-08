2a102337d0d8f1fdfd90c09bcda3188225bb384b
Gonzalo <gon.pineiro@gmail.com>
2021-07-08 08:50:18 -0300

    :hammer: add copyfile.php

--- public/views/formularios/copyfiles.php
+++ public/views/formularios/copyfiles.php
@@ -1,1 +1,26 @@
 <?php
+/* Update solicitudes with paths */
+$tituloController = new TituloController();
+foreach ($_FILES['imagenTitulos']['tmp_name'] as $key => $unaImagen) {
+    $fileType = $_FILES['imagenTitulos']['type'][$key];
+    $pathTítulo = getDireccionesParaAdjunto($fileType, $idSolicitud, $_POST['titulos'][$key], 'titulos', $key);
+    $solicitudUpdated = $tituloController->store(
+        [
+            'id_solicitud' => $idSolicitud,
+            'titulo' => $_POST['titulos'][$key],
+            'path_file' => $pathTítulo,
+            'es_curso' => null
+        ],
+        $idSolicitud
+    );
+    if (!$solicitudUpdated) {
+        $errores[] = "Solicitud nro $idSolicitud: Falla en update comprobante pago";
+        cargarLog($usuario['id'], $idSolicitud, $idCapacitador, "Solicitud nro $idSolicitud: Falla en update comprobante pago");
+    }
+
+    /* upload comprobante & certificado */
+    if (!copy($unaImagen, $pathTítulo)) {
+        $errores[] = "Solicitud nº $idSolicitud: Guardado de adjunto comprobante pago fallida";
+        cargarLog($usuario['id'], $idSolicitud, $idCapacitador, "Solicitud nº $idSolicitud: Guardado de adjunto comprobante pago fallida");
+    }
+}
--- public/views/formularios/inscripcion.php
+++ public/views/formularios/inscripcion.php
@@ -112,32 +112,7 @@ if (isset($_POST) && !empty($_POST)) {
             // Carga de imagenes en titulo
             if (isset($idSolicitud) && $idSolicitud != (false or null)) {
 
-
-                /* Update solicitudes with paths */
-                $tituloController = new TituloController();
-                foreach ($_FILES['imagenTitulos']['tmp_name'] as $key => $unaImagen) {
-                    $fileType = $_FILES['imagenTitulos']['type'][$key];
-                    $pathTítulo = getDireccionesParaAdjunto($fileType, $idSolicitud, $_POST['titulos'][$key], 'titulos', $key);
-                    $solicitudUpdated = $tituloController->store(
-                        [
-                            'id_solicitud' => $idSolicitud,
-                            'titulo' => $_POST['titulos'][$key],
-                            'path_file' => $pathTítulo,
-                            'es_curso' => null
-                        ],
-                        $idSolicitud
-                    );
-                    if (!$solicitudUpdated) {
-                        $errores[] = "Solicitud nro $idSolicitud: Falla en update comprobante pago";
-                        cargarLog($usuario['id'], $idSolicitud, $idCapacitador, "Solicitud nro $idSolicitud: Falla en update comprobante pago");
-                    }
-
-                    /* upload comprobante & certificado */
-                    if (!copy($unaImagen, $pathTítulo)) {
-                        $errores[] = "Solicitud nº $idSolicitud: Guardado de adjunto comprobante pago fallida";
-                        cargarLog($usuario['id'], $idSolicitud, $idCapacitador, "Solicitud nº $idSolicitud: Guardado de adjunto comprobante pago fallida");
-                    }
-                }
+                include('./copyfiles.php');
             } else {
                 $errores[] = 'Error en alta de solicitud';
                 //cargarLog($usuario['id'], $idSolicitud, $idCapacitador, 'Error en alta de solicitud');
