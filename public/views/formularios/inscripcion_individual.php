ad8e7638f39e6510b015407b8589de598318eb85
Norbert Stange <n0rb3r7@outlook.com>
2021-07-08 14:08:26 -0300

    Arreglo tabs activa por estado.

--- public/views/formularios/inscripcion_individual.php
+++ public/views/formularios/inscripcion_individual.php
@@ -56,19 +56,19 @@ if ($errores) {
             </li>
         </ul><!-- Tab panes -->
         <div class="tab-content">
-            <div class="tab-pane active" id="tabs-1" role="tabpanel">
+            <div class="tab-pane <?PHP if($estado_inscripcion == "DatosPersonales"){echo "active";} ?>" id="tabs-1" role="tabpanel">
                 <?PHP include('./01_personales.php'); ?>
             </div>
-            <div class="tab-pane" id="tabs-2" role="tabpanel">
+            <div class="tab-pane <?PHP if($estado_inscripcion == "Titulos"){echo "active";} ?>" id="tabs-2" role="tabpanel">
                 <?PHP include('./02_titulos.php'); ?>
             </div>
-            <div class="tab-pane" id="tabs-3" role="tabpanel">
+            <div class="tab-pane <?PHP if($estado_inscripcion == "Trabajos"){echo "active";} ?>" id="tabs-3" role="tabpanel">
                 <?PHP include('./03_trabajos.php'); ?>
             </div>
-            <div class="tab-pane" id="tabs-4" role="tabpanel">
+            <div class="tab-pane <?PHP if($estado_inscripcion == "Actividades"){echo "active";} ?>" id="tabs-4" role="tabpanel">
                 <?PHP include('./04_actividades.php'); ?>
             </div>
-            <div class="tab-pane" id="tabs-5" role="tabpanel">
+            <div class="tab-pane <?PHP if($estado_inscripcion == "Condiciones"){echo "active";} ?>" id="tabs-5" role="tabpanel">
                 <?PHP include('./05_condiciones.php'); ?>
             </div>
         </div>
