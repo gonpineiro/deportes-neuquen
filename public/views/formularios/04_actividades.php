b0dde9d1e8275a69476959a520b6f89e2fa3feea
Norbert Stange <n0rb3r7@outlook.com>
2021-07-19 11:18:11 -0300

    Paso actividades template.

--- public/views/formularios/04_actividades.php
+++ public/views/formularios/04_actividades.php
@@ -3,30 +3,115 @@
         <h4 class="text-white">Experiencia Laboral</h4>
         <hr>
         <!-- SELECCIÓN ACTIVIDADES LABORALES -->
-        <div class="form-group col-lg-6 col-md-6 col-sd-12 col-xs-12 ">
-            <label for="actividades-laborales" class="required">Seleccionar actividad laboral (una o varias) </label>
-            <div id="actividades-laborales">
-                <div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" values="1" name="actividades[]">
-                    <label class="custom-control-label" for="customCheck1">Una actividad</label>
-                </div>
+        <div class="form-group">
+            <h5 class="text-white pt-3">Deportes de calificación y votación</h5>
+            <div class="row" id="actividades-laborales">
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="1" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck1">Boxeo</label>
+                    </div>
 
-                <div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" values="2" name="actividades[]">
-                    <label class="custom-control-label" for="customCheck2">Otra actividad</label>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="2" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck2">Lucha Deportiva</label>
+                    </div>
                 </div>
-                <div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" values="3" name="actividades[]">
-                    <label class="custom-control-label" for="customCheck3">Una actividad</label>
-                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="3" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck3">Lucha Sumo</label>
+                    </div>
 
-                <div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" values="4" name="actividades[]">
-                    <label class="custom-control-label" for="customCheck4">Otra actividad</label>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="4" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck4">Lucha Sambo</label>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="5" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck3">Judo</label>
+                    </div>
+
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="6" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck4">Karate</label>
+                    </div>
+
                 </div>
             </div>
-            <div class="invalid-feedback">
-                Por favor ingrese las actividades.
+            <h5 class="text-white pt-3">Deportes de anotación</h5>
+            <div class="row" id="actividades-laborales">
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="7" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck1">Taekkwondo</label>
+                    </div>
+
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="8" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck2">Otra actividad</label>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="3" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck3">Una actividad</label>
+                    </div>
+
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="4" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck4">Otra actividad</label>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="3" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck3">Una actividad</label>
+                    </div>
+
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="4" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck4">Otra actividad</label>
+                    </div>
+                </div>
+            </div>
+            <h5 class="text-white pt-3">Deportes de medición</h5>
+            <div class="row" id="actividades-laborales">
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="1" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck1">Una actividad</label>
+                    </div>
+
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="2" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck2">Otra actividad</label>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="3" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck3">Una actividad</label>
+                    </div>
+
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="4" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck4">Otra actividad</label>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="3" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck3">Una actividad</label>
+                    </div>
+
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" values="4" name="actividades[]">
+                        <label class="custom-control-label" for="customCheck4">Otra actividad</label>
+                    </div>
+                </div>
             </div>
         </div>
         <input class="btn btn-primary mt-3 mb-3" type="submit" id="submit" value="Registrar datos" name="actividadesSubmit" />
