7b8cd70c5e5d8f18327108547ff62526eb22d72d
Gonzalo <gon.pineiro@gmail.com>
2021-07-19 13:40:32 -0300

    :hammer: BarrioModelErrors

--- app/models/Barrio.php
+++ app/models/Barrio.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This is the model class for table "Barrios".
  *
@@ -32,9 +33,7 @@ class Barrio
         /* Guardamos los errores */
         if ($conn->getError()) {
             $error =  $conn->getError() . ' | Error al guardar un barrio';
-            $log = new Log();
-            $log->set($this->nombre, null, null, $error, get_class(), 'save');
-            $log->save();
+            cargarLog(null, null, $error, get_class(), __FUNCTION__);
         }
         return $result;
     }
@@ -47,9 +46,7 @@ class Barrio
         /* Guardamos los errores */
         if ($conn->getError()) {
             $error =  $conn->getError() . ' | Error al listar los barrios';
-            $log = new Log();
-            $log->set(null, null, null, $error, get_class(), 'list');
-            $log->save();
+            cargarLog(null, null, $error, get_class(), __FUNCTION__);
         }
         return $result;
     }
@@ -63,9 +60,7 @@ class Barrio
         /* Guardamos los errores */
         if ($conn->getError()) {
             $error =  $conn->getError() . ' | Error a obtener el barrio: ' . $params[0];
-            $log = new Log();
-            $log->set(null, null, null, $error, get_class(), 'get');
-            $log->save();
+            cargarLog(null, null, $error, get_class(), __FUNCTION__);
         }
         return $barrio;
     }
@@ -77,10 +72,8 @@ class Barrio
 
         /* Guardamos los errores */
         if ($conn->getError()) {
-            $error =  $conn->getError() . ' | Error a modificar el barrio';
-            $log = new Log();
-            $log->set(null,  $id, null, $error, get_class(), 'update');
-            $log->save();
+            $error =  $conn->getError() . ' | Error a modificar el barrio ' . $id;
+            cargarLog(null, null, $error, get_class(), __FUNCTION__);
         }
         return $result;
     }
