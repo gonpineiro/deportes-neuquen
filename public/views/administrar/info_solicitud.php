6e5c372a6b82cd7592de5bf1eba79ca64565c7a2
Norbert Stange <n0rb3r7@outlook.com>
2021-08-23 13:37:12 -0300

    Primera tab con visualización de deatos personales.
    - Estructura del resto de tabs.

--- public/views/administrar/info_solicitud.php
+++ public/views/administrar/info_solicitud.php
@@ -2,5 +2,80 @@
 
 if ($_GET['id']) {
     $id_solicitud = $_GET['id'];
-    echo $id_solicitud;
 }
+?>
+
+<?php
+include '../../../app/config/config.php';
+
+if (!isset($_SESSION['usuario'])) {
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . WEBLOGIN);
+    exit();
+}
+/* datos de la sesion */
+include('../common/session.php');
+// datos de la solicitud
+$sol_nombre = "Nombre Apellido";
+$sol_email = "mail@mail.com";
+$sol_celular = 54654654;
+$sol_nacionalidad = "Argentina";
+$sol_ciudad = "Neuquén";
+$sol_barrio = "Barrio X";
+$sol_cod_postal = 8300;
+$sol_dire_calle = "Evergreen";
+$sol_dire_numero = 3500;
+$sol_dire_depto = 1015;
+$sol_dire_piso = 5;
+$sol_antecedentes = "../../BASES_Y_CONDICIONES.pdf";
+
+?>
+
+<?php include('../common/header.php'); ?>
+
+<div class="body container" style="padding-bottom: 50px;">
+    <?php include('../common/navbar.php'); ?>
+    <div style="min-height: 50px;">
+        <h2 style="padding:30px 0px;color: #076AB3;">INFORMACIÓN SOLICITUD Nº <?= $id_solicitud ?></h2>
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <a class="nav-link active" id="datosPersonales-tab" data-toggle="tab" href="#datosPersonales" role="tab" aria-controls="datosPersonales" aria-selected="true">Datos Personales</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" id="formacionAcademica-tab" data-toggle="tab" href="#formacionAcademica" role="tab" aria-controls="formacionAcademica" aria-selected="false">Formación Académica</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" id="lugarTrabajo-tab" data-toggle="tab" href="#lugarTrabajo" role="tab" aria-controls="lugarTrabajo" aria-selected="false">Lugar Trabajo</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" id="actividades-tab" data-toggle="tab" href="#actividades" role="tab" aria-controls="actividades" aria-selected="false">Actividades</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" id="condiciones-tab" data-toggle="tab" href="#condiciones" role="tab" aria-controls="condiciones" aria-selected="false">Condiciones</a>
+            </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="datosPersonales" role="tabpanel" aria-labelledby="datosPersonales-tab">
+                <?php include('./tabs/datosPersonales.php'); ?>
+            </div>
+            <div class="tab-pane fade" id="formacionAcademica" role="tabpanel" aria-labelledby="formacionAcademica-tab">
+                <?php include('./tabs/formacionAcademica.php'); ?>
+            </div>
+            <div class="tab-pane fade" id="lugarTrabajo" role="tabpanel" aria-labelledby="lugarTrabajo-tab">
+                <?php include('./tabs/lugarTrabajo.php'); ?>
+            </div>
+            <div class="tab-pane fade" id="actividades" role="tabpanel" aria-labelledby="actividades-tab">
+                <?php include('./tabs/actividades.php'); ?>
+            </div>
+            <div class="tab-pane fade" id="condiciones" role="tabpanel" aria-labelledby="condiciones-tab">
+                <?php include('./tabs/condiciones.php'); ?>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+</div>
+
+<?php include('./components/footer.php'); ?>
---
+++ public/views/administrar/tabs/datosPersonales.php
@@ -0,0 +1,106 @@
+<div class="form-horizontal mx-auto needs-validation" name="form-1" id="form-1" novalidate>
+    <div class="card-body mb-5" style="border-bottom-right-radius: 20px;border-bottom-left-radius:20px;">
+        <!-- DATOS PERSONALES -->
+        <h3 class="text-white"><?= $sol_nombre ?></h3>
+        <h4 class="text-white">Datos personales</h4>
+        <hr>
+        <div class="form-group row">
+            <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12">
+                <label for="email">Direcci&oacute;n de email </label>
+                <input disabled type="email" id="email" class="form-control" value="<?= $sol_email ?>">
+            </div>
+            <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12">
+                <label for="telefono">Actualice su n&uacute;mero de tel&eacute;fono </label>
+                <input disabled type="number" id="telefono" class="form-control" value="<?= $sol_celular; ?>">
+
+            </div>
+            <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12">
+                <label for="nacionalidad">Nacionalidad </label>
+                <input disabled id="nacionalidad" class="form-control" value="<?= $sol_nacionalidad ?>" style="display:block !important;">
+            </div>
+            <!-- DOMICILIO -->
+            <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12">
+                <label for="ciudad">Seleccione su ciudad </label>
+                <input disabled id="ciudad" class="form-control" value="<?= $sol_ciudad ?>" title="Seleccionar" name='ciudad' style="display:block !important;" required>
+                <div class="invalid-feedback">
+                    Por favor indique su ciudad.
+                </div>
+            </div>
+            <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12">
+                <label for="barrio-nqn">Seleccione su barrio </label>
+                <input disabled id="barrio-nqn" class="form-control" value="<?= $sol_barrio ?>">
+            </div>
+        </div>
+        <!-- BARRIO -->
+        <div class="form-group row">
+            <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12 ">
+                <label for="direccion-cp">Código Postal </label>
+                <input disabled id="direccion-cp" class="form-control" value="<?= $sol_cod_postal ?>">
+            </div>
+            <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12 ">
+                <label for="direccion-calle">Calle </label>
+                <input disabled id="direccion-calle" class="form-control" value="<?= $sol_dire_calle ?>">
+            </div>
+            <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12 ">
+                <label for="direccion-numero">Número </label>
+                <input disabled id="direccion-numero" class="form-control" value="<?= $sol_dire_numero ?>">
+            </div>
+            <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12 ">
+                <label for="direccion-departamento">Departamento (opcional) </label>
+                <input disabled id="direccion-departamento" class="form-control" value="<?= $sol_dire_depto ?>">
+
+            </div>
+            <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12 ">
+                <label for="direccion-piso">Piso (opcional) </label>
+                <input disabled id="direccion-piso" class="form-control" value="<?= $sol_dire_piso ?>">
+
+            </div>
+        </div>
+        <hr>
+        <div class="form-group row">
+            <div class="form-group col-lg-6 col-md-6 col-sd-12 col-xs-12 ">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#antecedentesModal">Ver Antecedentes</button>
+            </div>
+        </div>
+        <hr>
+        <div class="buttonsRow container">
+            <div class="row">
+                <div class="col-md-12 my-1">
+                    <div class="float-md-right float-xs-left">
+                        <input class="btn btn-primary" type="submit" disabled id="submitBtn1" value="Confirmar y Guardar" name="personalesSubmit" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- POR SI LO USAMOS. BORRAR SI NO SE USA :D-->
+        <div class="process" style="display: none;">
+            <div class="spinner-border text-light" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Atecedentes-->
+<div class="modal fade" id="antecedentesModal" tabindex="-1" aria-labelledby="antecedentesModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="antecedentesModalLabel">Antecedentes Penales</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <iframe id="archivo_antecedentes" title="Visor Antecedentes" width="100%" height="400" src="<?= $sol_antecedentes ?>">
+                </iframe>
+
+            </div>
+            <div class="modal-footer">
+                <a type="button" href="<?= $sol_antecedentes ?>" download="antecedentes_<?= $sol_nombre ?>" target="_blank" class="btn btn-primary">Descargar</a>
+
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
