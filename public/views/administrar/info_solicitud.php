7f69b71eb2f884511e231bc837c20cf49b6be5f2
Norbert Stange <n0rb3r7@outlook.com>
2021-08-24 13:50:57 -0300

    Cambio visualización info solicitud.

--- public/views/administrar/info_solicitud.php
+++ public/views/administrar/info_solicitud.php
@@ -23,53 +23,6 @@ if (isset($_GET['id'])) {
 
     $actividades = $solicitud['actividades'];
 }
-
-// datos de la solicitud
-$sol_nombre = "Nombre Apellido";
-$sol_email = "mail@mail.com";
-$sol_celular = 54654654;
-$sol_nacionalidad = "Argentina";
-$sol_ciudad = "Neuquén";
-$sol_barrio = "Barrio X";
-$sol_cod_postal = 8300;
-$sol_dire_calle = "Evergreen";
-$sol_dire_numero = 3500;
-$sol_dire_depto = 1015;
-$sol_dire_piso = 5;
-$sol_antecedentes = "../../BASES_Y_CONDICIONES.pdf";
-$sol_titulo = [
-    0 => [
-        "id" => "1",
-        "titulo" => "Licenciado en Educación Física",
-        "path_file" => "../../BASES_Y_CONDICIONES.pdf",
-    ],
-    1 => [
-        "id" => "2",
-        "titulo" => "Maestro en Judo",
-        "path_file" => "../../BASES_Y_CONDICIONES.pdf",
-    ],
-];
-$sol_trabajo = [
-    0 => [
-        "id" => "1",
-        "lugar" => "Ferraciolli",
-        "path_file" => "../../BASES_Y_CONDICIONES.pdf",
-    ],
-    1 => [
-        "id" => "2",
-        "lugar" => "Ectremo Deborte",
-        "path_file" => "../../BASES_Y_CONDICIONES.pdf",
-    ],
-];
-$sol_actividades =
-    [
-        0 => [
-            "nombre" => "Judo",
-        ],
-        1 => [
-            "nombre" => "Balonmano",
-        ],
-    ];
 ?>
 
 <?php include('../common/header.php'); ?>
@@ -78,38 +31,50 @@ $sol_actividades =
     <?php include('../common/navbar.php'); ?>
     <div style="min-height: 50px;">
         <h2 style="padding:30px 0px;color: #076AB3;">INFORMACIÓN SOLICITUD Nº <?= $id ?> - <?= $solicitud['nombre_te'] ?></h2>
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <li class="nav-item" role="presentation">
-                <a class="nav-link active" id="datosPersonales-tab" data-toggle="tab" href="#datosPersonales" role="tab" aria-controls="datosPersonales" aria-selected="true">Datos Personales</a>
-            </li>
-            <li class="nav-item" role="presentation">
-                <a class="nav-link" id="formacionAcademica-tab" data-toggle="tab" href="#formacionAcademica" role="tab" aria-controls="formacionAcademica" aria-selected="false">Formación Académica</a>
-            </li>
-            <li class="nav-item" role="presentation">
-                <a class="nav-link" id="lugarTrabajo-tab" data-toggle="tab" href="#lugarTrabajo" role="tab" aria-controls="lugarTrabajo" aria-selected="false">Lugar Trabajo</a>
-            </li>
-            <li class="nav-item" role="presentation">
-                <a class="nav-link" id="actividades-tab" data-toggle="tab" href="#actividades" role="tab" aria-controls="actividades" aria-selected="false">Actividades</a>
-            </li>
-            <li class="nav-item" role="presentation">
-                <a class="nav-link" id="condiciones-tab" data-toggle="tab" href="#condiciones" role="tab" aria-controls="condiciones" aria-selected="false">Condiciones</a>
-            </li>
-        </ul>
-        <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="datosPersonales" role="tabpanel" aria-labelledby="datosPersonales-tab">
-                <?php include('./tabs/datosPersonales.php'); ?>
+        <div class="row">
+            <div class="col-12 col-md-3">
+                <div class="card-body mb-5" style="border-bottom-right-radius: 20px;border-bottom-left-radius:20px;border-top-left-radius: .25rem;border-top-right-radius: .25rem;">
+                    <img src=<?= $solicitud['foto'] ?> class="rounded mx-auto d-block px-2" style="width: 100%;" alt="">
+                    <h5 class="text-white text-center pt-4"><?= $solicitud['nombre_te'] ?></h5>
+                    <p><i class="bi bi-envelope-fill"></i> <?= $solicitud['email'] ?></p>
+                    <p><i class="bi bi-phone-fill"></i> <?= $solicitud['telefono'] ?></p>
+                </div>
             </div>
-            <div class="tab-pane fade" id="formacionAcademica" role="tabpanel" aria-labelledby="formacionAcademica-tab">
-                <?php include('./tabs/formacionAcademica.php'); ?>
-            </div>
-            <div class="tab-pane fade" id="lugarTrabajo" role="tabpanel" aria-labelledby="lugarTrabajo-tab">
-                <?php include('./tabs/lugarTrabajo.php'); ?>
-            </div>
-            <div class="tab-pane fade" id="actividades" role="tabpanel" aria-labelledby="actividades-tab">
-                <?php include('./tabs/actividades.php'); ?>
-            </div>
-            <div class="tab-pane fade" id="condiciones" role="tabpanel" aria-labelledby="condiciones-tab">
-                <?php include('./tabs/condiciones.php'); ?>
+            <div class="col-12 col-md-9">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link active" id="datosPersonales-tab" data-toggle="tab" href="#datosPersonales" role="tab" aria-controls="datosPersonales" aria-selected="true">Datos Personales</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="formacionAcademica-tab" data-toggle="tab" href="#formacionAcademica" role="tab" aria-controls="formacionAcademica" aria-selected="false">Formación Académica</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="lugarTrabajo-tab" data-toggle="tab" href="#lugarTrabajo" role="tab" aria-controls="lugarTrabajo" aria-selected="false">Lugar Trabajo</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="actividades-tab" data-toggle="tab" href="#actividades" role="tab" aria-controls="actividades" aria-selected="false">Actividades</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="condiciones-tab" data-toggle="tab" href="#condiciones" role="tab" aria-controls="condiciones" aria-selected="false">Condiciones</a>
+                    </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="datosPersonales" role="tabpanel" aria-labelledby="datosPersonales-tab">
+                        <?php include('./tabs/datosPersonales.php'); ?>
+                    </div>
+                    <div class="tab-pane fade" id="formacionAcademica" role="tabpanel" aria-labelledby="formacionAcademica-tab">
+                        <?php include('./tabs/formacionAcademica.php'); ?>
+                    </div>
+                    <div class="tab-pane fade" id="lugarTrabajo" role="tabpanel" aria-labelledby="lugarTrabajo-tab">
+                        <?php include('./tabs/lugarTrabajo.php'); ?>
+                    </div>
+                    <div class="tab-pane fade" id="actividades" role="tabpanel" aria-labelledby="actividades-tab">
+                        <?php include('./tabs/actividades.php'); ?>
+                    </div>
+                    <div class="tab-pane fade" id="condiciones" role="tabpanel" aria-labelledby="condiciones-tab">
+                        <?php include('./tabs/condiciones.php'); ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- public/views/administrar/tabs/datosPersonales.php
+++ public/views/administrar/tabs/datosPersonales.php
@@ -1,63 +1,61 @@
 <div class="form-horizontal mx-auto">
     <div class="card-body mb-5" style="border-bottom-right-radius: 20px;border-bottom-left-radius:20px;">
         <!-- DATOS PERSONALES -->
-        <h3 class="text-white"><?= $solicitud['nombre_te'] ?></h3>
         <h4 class="text-white">Datos personales</h4>
         <hr>
         <div class="form-group row">
-            <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12">
-                <label for="email">Direcci&oacute;n de email </label>
-                <input disabled type="email" id="email" class="form-control" value="<?= $solicitud['email'] ?>">
+            <!-- <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12">
+                <label for="email"><strong>Direcci&oacute;n de email</strong> </label>
+                <input disabled readonly type="email" id="email" class="form-control" value="<?= $solicitud['email'] ?>">
             </div>
             <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12">
                 <label for="telefono">Actualice su n&uacute;mero de tel&eacute;fono </label>
-                <input disabled type="number" id="telefono" class="form-control" value="<?= $solicitud['telefono'] ?>">
-
-            </div>
+                <input disabled readonly type="number" id="telefono" class="form-control" value="<?= $solicitud['telefono'] ?>">
+            </div> -->
             <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12">
                 <label for="nacionalidad">Nacionalidad </label>
-                <input disabled id="nacionalidad" class="form-control" value="<?= $solicitud['nacionalidad'] ?>" style="display:block !important;">
+                <input disabled readonly id="nacionalidad" class="form-control" value="<?= $solicitud['nacionalidad'] ?>" style="display:block !important;">
             </div>
             <!-- DOMICILIO -->
             <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12">
                 <label for="ciudad">Seleccione su ciudad </label>
-                <input disabled id="ciudad" class="form-control" value="<?= $solicitud['ciudad'] ?>" title="Seleccionar" name='ciudad' style="display:block !important;" required>
+                <input disabled readonly id="ciudad" class="form-control" value="<?= $solicitud['ciudad'] ?>" title="Seleccionar" name='ciudad' style="display:block !important;" required>
                 <div class="invalid-feedback">
                     Por favor indique su ciudad.
                 </div>
             </div>
             <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12">
                 <label for="barrio-nqn">Seleccione su barrio </label>
-                <input disabled id="barrio-nqn" class="form-control" value="<?= $solicitud['barrio'] ?>">
+                <input disabled readonly id="barrio-nqn" class="form-control" value="<?= $solicitud['barrio'] ?>">
             </div>
         </div>
         <!-- BARRIO -->
         <div class="form-group row">
             <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12 ">
                 <label for="direccion-cp">Código Postal </label>
-                <input disabled id="direccion-cp" class="form-control" value="<?= $solicitud['cp'] ?>">
+                <input disabled readonly id="direccion-cp" class="form-control" value="<?= $solicitud['cp'] ?>">
             </div>
             <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12 ">
                 <label for="direccion-calle">Calle </label>
-                <input disabled id="direccion-calle" class="form-control" value="<?= $solicitud['calle'] ?>">
+                <input disabled readonly id="direccion-calle" class="form-control" value="<?= $solicitud['calle'] ?>">
             </div>
             <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12 ">
                 <label for="direccion-numero">Número </label>
-                <input disabled id="direccion-numero" class="form-control" value="<?= $solicitud['nro_calle'] ?>">
+                <input disabled readonly id="direccion-numero" class="form-control" value="<?= $solicitud['nro_calle'] ?>">
             </div>
             <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12 ">
                 <label for="direccion-departamento">Departamento (opcional) </label>
-                <input disabled id="direccion-departamento" class="form-control" value="<?= $solicitud['depto'] ?>">
+                <input disabled readonly id="direccion-departamento" class="form-control" value="<?= $solicitud['depto'] ?>">
 
             </div>
             <div class="form-group col-lg-4 col-md-4 col-sd-12 col-xs-12 ">
                 <label for="direccion-piso">Piso (opcional) </label>
-                <input disabled id="direccion-piso" class="form-control" value="<?= $solicitud['piso'] ?>">
+                <input disabled readonly id="direccion-piso" class="form-control" value="<?= $solicitud['piso'] ?>">
 
             </div>
         </div>
         <hr>
-        <div class="form-group row">
+        <div class="form-group row pt-3">
             <div class="form-group col-lg-6 col-md-6 col-sd-12 col-xs-12 ">
                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#antecedentesModal">Ver Antecedentes</button>
             </div>
@@ -67,7 +65,7 @@
             <div class="row">
                 <div class="col-md-12 my-1">
                     <div class="float-md-right float-xs-left">
-                        <input class="btn btn-primary" type="submit" disabled id="submitBtn1" value="Confirmar y Guardar" name="personalesSubmit" />
+                        <input class="btn btn-primary" type="submit" disabled readonly id="submitBtn1" value="Confirmar y Guardar" name="personalesSubmit" />
                     </div>
                 </div>
             </div>
