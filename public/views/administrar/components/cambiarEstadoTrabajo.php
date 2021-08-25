9c93ac7ba3630c006c4cbcd8e6bdef723bf1b7d4
Norbert Stange <n0rb3r7@outlook.com>
2021-08-25 11:58:36 -0300

    Cambio estado en tab trabajos.

---
+++ public/views/administrar/components/cambiarEstadoTrabajo.php
@@ -0,0 +1,29 @@
+<?php
+
+include '../../../../app/config/config.php';
+
+if (!isset($_SESSION['usuario'])) {
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . WEBLOGIN);
+    exit();
+}
+
+$trabajoController = new TrabajoController();
+
+if (isset($_POST) && !empty($_POST)) {
+    $id_trabajo = $_POST['id_titulo'];
+    $id_solicitud = $_POST['id_solicitud'];
+    if (isset($_POST['rechazarBtn'])) {
+        $estado = "Rechazado";
+    }
+    if (isset($_POST['aprobarBtn'])) {
+        $estado = "Aprobado";
+    }
+
+    $trabajoController->update(['estado' => $estado], $id_trabajo);
+
+    header('Location: ../info_solicitud.php?id=' .  $id_solicitud);
+    exit();
+} else {
+    exit();
+}
